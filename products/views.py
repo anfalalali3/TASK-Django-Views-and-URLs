2c21489f040e8095626418c14dd741610963dc38
Anfal <ahassan2@mof.gov.kw>
2022-10-15 19:00:48 +0300

    task templates Detail Page done

--- products/views.py
+++ products/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Product
+
 # Create your views here.
+def get_home (request):
+    return HttpResponse("<h1>Welcome to the website<h1>")
+
+def get_product (request, product_id):
+    product = Product.objects.get(id=product_id)
+    context={
+        "product":{
+            "name":product.name,
+            "description":product.description,
+            "price":product.price
+        }
+
+    }
+    return render(request,"product_detail.html",context)
